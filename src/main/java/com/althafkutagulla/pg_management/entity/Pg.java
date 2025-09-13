710042211a806d1e4f7f18645e07eb6d777e688b
Althaf Kutagulla <111273306+Althaf-Kutagulla@users.noreply.github.com>
2025-09-13 14:00:28 +0530

    pg entity

---
+++ src/main/java/com/althafkutagulla/pg_management/entity/Pg.java
@@ -0,0 +1,22 @@
+package com.althafkutagulla.pg_management.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
+public class Pg {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String name;
+}
